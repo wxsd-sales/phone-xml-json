cda21c44da2c1d9f76f1ba1e533a9d2ffe346d26
Taylor Hanson <tahanson@cisco.com>
2025-08-14 15:24:40 -0400

    code update for user timezones

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const __dirname = dirname(__filename);
 console.log(path.join(__dirname, 'src'));
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'src')));
-app.use(express.json());
+//app.use(express.json());
 
 var router = express.Router();
 // simple logger for this router's requests
@@ -42,7 +42,7 @@ function returnXML(pagePrompt, defaultBadge){
     <CiscoIPPhoneInput>
         <Title>${process.env.APP_TITLE}</Title>
         <Prompt>${pagePrompt}</Prompt>
-        <URL>${returnUrl}/default/none</URL>
+        <URL>${returnUrl}/default/none/null</URL>
         <InputItem>
             <DisplayName>Badge</DisplayName>
             <QueryStringParam>badge</QueryStringParam>
@@ -76,7 +76,7 @@ function returnText(responseText, prompt){
   `;
 }
 
-function confirmXML(badge, name){
+function confirmXML(badge, name, timezone){
   return `
       <CiscoIPPhoneText>
         <Title>${process.env.APP_TITLE}</Title>
@@ -84,7 +84,7 @@ function confirmXML(badge, name){
         <Text>${name}</Text>
         <SoftKeyItem>
           <Name>Submit</Name>
-          <URL>${returnUrl}/punch/${encodeURIComponent(name)}?badge=${badge}</URL>
+          <URL>${returnUrl}/punch/${encodeURIComponent(name)}/${timezone}?badge=${badge}</URL>
           <Position>3</Position>
         </SoftKeyItem>
         <SoftKeyItem>
@@ -107,7 +107,7 @@ async function authenticate(){
       'realm':process.env.REALM,
       'audience':process.env.AUDIENCE
   });
-  console.log(body);
+  //console.log(body);
   let resp = await fetch(process.env.ROPCURL,{
     method: "POST",
     headers:{'Content-Type': 'application/x-www-form-urlencoded'},
@@ -136,6 +136,7 @@ async function getPerson(badge){
     returnVal.message = resp.statusText;
     let json = await resp.json();
     console.log('getPerson response json:', json);
+    console.log('getPerson response timezoneId:', json.employeeExtension?.timeZoneId);
     if(json.errorCode && json.message){
       returnVal.message = json.message;
       if(json.details){
@@ -145,6 +146,7 @@ async function getPerson(badge){
     if(resp.status >= 200 && resp.status < 300){
       returnVal.success = true;
       returnVal.message = json.employeeExtension?.fullName;
+      returnVal.timezone = json.employeeExtension?.timeZoneId;
     }
   }catch(e){
     console.error("getPerson error:");
@@ -153,10 +155,29 @@ async function getPerson(badge){
   return returnVal
 }
 
-async function punch(badge){
+async function getTimezone(timezoneId){
+  let timezoneObject;
+  try{
+    let resp = await fetch(`${process.env.POST_URL}/api/v1/commons/setup/timezones/${timezoneId}`,{
+      method: "GET",
+      headers:{"Authorization":accessToken, "Content-Type":"application/json"}
+    });
+    console.log('getTimezone response status:', resp.status);
+    console.log('getTimezone response statusText:', resp.statusText);
+    timezoneObject = await resp.json();
+    console.log('getTimezone response json:', timezoneObject);
+  }catch(e){
+    console.error("getTimezone error:");
+    console.error(e);
+  }
+  return timezoneObject
+}
+
+async function punch(badge, timezoneId){
   let returnVal = {success:false, message:"Error"};
   try{
-    let now = new Date().toISOString();
+    let now = await getUserTime(timezoneId);
+    now = now.toISOString();
     now = now.slice(0,19);
     let payload = {
         "punches": [{
@@ -166,9 +187,9 @@ async function punch(badge){
                 }
             }]
     }
-    console.log('accessToken', accessToken);
     console.log('punch payload:');
     console.log(JSON.stringify(payload, null, 2));
+    
     let resp = await fetch(`${process.env.POST_URL}/api/v1/timekeeping/punches/import`,{
       method: "POST",
       headers:{"Authorization":accessToken, "Content-Type":"application/json"},
@@ -195,6 +216,79 @@ async function punch(badge){
   return returnVal;
 }
 
+async function getUserTime(timezoneId){
+  let now = new Date();
+  let userTimezone = await getTimezone(timezoneId);
+  try {
+    now.setSeconds(now.getSeconds() + (userTimezone.offsetMinutes*60));
+
+    let workingTimezone = userTimezone.effectiveDatedTimeZoneList.at(-1);
+    if(workingTimezone.dstStart && workingTimezone.dstEnd){
+      let tz = workingTimezone.timezone;
+      console.log('tz', tz);
+      let startDst = nthWeekdayOfMonth(tz.startDayOfWeek-1, tz.startDay, tz.startMonth, tz.startTime);
+      let endDst = nthWeekdayOfMonth(tz.endDayOfWeek-1, tz.endDay, tz.endMonth, tz.endTime);
+      
+      let dst = isDST(startDst, endDst, now);
+      console.log('dst', dst);
+      if(dst){//Add 1 more hour for DST
+        now.setSeconds(now.getSeconds() + 3600);
+      }
+    }
+  } catch(e){
+    console.error('getUserTime error: ', e);
+  }
+  return now;
+}
+
+function isDST(start, end, check){
+  return check > start && check < end;
+}
+
+function nthWeekdayOfMonth(weekday, n, month, startTime) {
+  //weekday: 0 - 6 (Sunday - Saturday)
+  //n: 1 = first, 2 = second, -1 = last, -2 = second to last
+  //month: 0 - 11 (Jan - Dec)
+  //startTime = time of day in miliseconds, like 7200000 for 2:00 am UTC
+  // returns corresponding date for the current year, example:
+  // nthWeekdayOfMonth(2, 1, 8, 72000) //First Tuesday of September (current year is 2025)
+  // returns: 2025-09-02T04:00:00.000Z
+  let date = new Date()
+  const thisYear = date.toISOString().slice(0,4)
+  var count = 0;
+  var idate;
+  let positive = true;
+  if(n > 0){
+    idate = new Date(thisYear, month, 1, 0, 0, 0, startTime);
+  } else {
+    positive = false;
+    idate = new Date(thisYear, month+1, 0, 0, 0, 0, startTime);
+  }
+  
+  let startMonth = idate.getMonth();
+  while (true) {
+    if(idate.getMonth() !== startMonth){
+      return;
+    }
+    if(positive){
+      if (idate.getDay() === weekday) {
+        if (++count == n) {
+          break;
+        }
+      }
+      idate.setDate(idate.getDate() + 1);
+    } else {
+      if (idate.getDay() === weekday) {
+        if (--count == n) {
+          break;
+        }
+      }
+      idate.setDate(idate.getDate() - 1);
+    }
+  }
+  return idate;
+}
+
 router.get('/', (req, res) => {
   console.log(req.headers);
   res.setHeader('Content-Type',"text/xml");
@@ -203,15 +297,14 @@ router.get('/', (req, res) => {
   res.send(xmlResponse);
 });
 
-router.get('/xml/:command/:name', async (req, res) => {
+router.get('/xml/:command/:name/:arg1', async (req, res) => {
   console.log(req.headers);
   let xmlResponse;
   console.log(req.path);
   console.log(req.query);
   if(req.query.badge){
-    //let result = await punch(req.query.badge);
     if(req.params.command === "punch"){
-      let result = await punch(req.query.badge);
+      let result = await punch(req.query.badge, req.params.arg1);
       if(result['success']){
         if(req.params.name){
           xmlResponse = returnText(`Punch recorded for ${req.params.name}`);
@@ -226,8 +319,7 @@ router.get('/xml/:command/:name', async (req, res) => {
       let result = await getPerson(req.query.badge);
       if(result['success']){
         console.log(result);
-        xmlResponse = confirmXML(req.query.badge, result["message"]);
-        //xmlResponse = returnText(result["message"].replace(",",""), "Submit to confirm");
+        xmlResponse = confirmXML(req.query.badge, result["message"], result["timezone"]);
       } else {
         xmlResponse = returnText(result["message"], 'Error');
       }
@@ -245,6 +337,7 @@ app.use(`/`, router);
 app.listen(port, async () => {
   let authentication = await authenticate();
   accessToken = authentication.access_token;
+  //console.log(accessToken);
   let intervalTime = authentication.expires_in - 300; // refresh 5 minutes sooner than we need to (300 seconds)
   setInterval(async function(){
     let authentication = await authenticate();
